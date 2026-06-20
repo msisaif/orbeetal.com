a7dfdf7afe33f56e3aeba51a832b4faea1c13d03
msisaif <msi313.bd@gmail.com>
2026-06-20 10:06:39 +0600

    feat: add remote image configuration for external domain
    - Updated next.config.mjs to include remotePatterns for images, allowing images from 'orbeetal.com' to be served securely.
    - This enhancement improves image handling and expands the capabilities for content management.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'orbeetal.com',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
